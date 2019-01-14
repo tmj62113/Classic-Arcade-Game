5b31a0e0c1b4edcba47d5db0affedcf9b5ff8797
Tiffany Jensen <39684784+tmj62113@users.noreply.github.com>
2019-01-14 13:08:02 -0600

    Add files via upload

---
+++ README.md
@@ -0,0 +1,25 @@
+
+# Introduction
+___
+This is a Udacity Front-end Nanodegree project, part of my Google Scholarship. The idea is to recreate a simple clone of the classic Frogger arcade game.
+
+# Run the application
+
+Open [Link](https://github.com/tmj62113/Classic-Arcade-Game) to run online
+
+<img src="/images/ArcadeGameScreenShot.png">
+
+## Installation
+
+If you would like to install this game on your device you can clone or download this repo. Once you have the project folder all you have to do is launch the `index.html`file in your browser (you can find an option to do so by right-clicking on the file). Enjoy
+
+# Play the game
+
+1. Moving player (left, right, up or down) by using arrow key on the keyboard against any enemy.
+
+2. If player have a collision with an enemy, the player will be back to a default position (at the bottom of game screen).
+
+3. If player reach the top of the game screen without any enemy's collision, the player wins.
+
+## Code
+Developed using Vanilla JS and Object Oriented JavaScript.
---
+++ app.js
@@ -0,0 +1,144 @@
+// Enemies our player must avoid
+var Enemy = function(x, y, speed) {
+    this.x = x;
+    this.y = y + 55; //center
+    this.speed = speed;
+    this.sprite = 'images/enemy-bug.png';
+    this.step = 101;
+    this.boundary = this.step * 5;
+    this.resetPos = -this.step;
+};
+
+// Update the enemy's position, required method for game
+// Parameter: dt, a time delta between ticks
+Enemy.prototype.update = function(dt) {
+    // You should multiply any movement by the dt parameter
+    // which will ensure the game runs at the same speed for
+    // all computers.
+
+    // If enemy is not passed boundary
+    if(this.x < this.boundary) {
+      // Move Forward
+      // Increment x by speed * dt
+      this.x += this.speed * dt;
+    }
+      else {
+        // Reset pos to start
+        this.x = this.resetPos;
+      }
+};
+
+// Draw the enemy on the screen, required method for game
+Enemy.prototype.render = function() {
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+};
+
+// Now write your own player class
+// This class requires an update(), render() and
+// a handleInput() method.
+
+// Hero class
+class Hero {
+  constructor() {
+      this.sprite = 'images/char-cat-girl.png';
+      this.step = 101;
+      this.jump = 83;
+      this.startX = this.step * 2;
+      this.startY = (this.jump * 4) + 55;
+      this.x = this.startX;
+      this.y = this.startY;
+      this.victory = false;
+    }
+
+  // Methods
+  // Update Position
+
+    update() {
+      // Check Collision here
+      for(let enemy of allEnemies) {
+        // Did player x and y collide with enemy?
+         if (this.y === enemy.y && (enemy.x + enemy.step/2 > this.x
+       && enemy.x < this.x + this.step/2) ) {
+            this.reset();
+        }
+      }
+
+      // Check Win here
+        // Did player x and y reach final tile?
+        if(this.y === 55) {
+          this.victory = true;
+        }
+      }
+  // Render
+      // Draw player sprite on current x and y coord position
+      render() {
+          ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+          }
+          /**
+           * Update player's x and y property according to input
+           *
+           *  @param {string} input - Directions to travel
+           */
+           handleInput(input) {
+             switch(input) {
+                 case 'left':
+                      if (this.x > 0) {
+                      this.x -= this.step;
+                      }
+                      break;
+                 case 'up':
+                      if (this.y > this.jump) {
+                          this.y -= this.jump;
+                      }
+                      break;
+                 case 'right':
+                      if (this.x < this.step * 4) {
+                          this.x += this.step;
+                      }
+                      break;
+                 case 'down':
+                      if (this.y < this.jump * 4) {
+                          this.y += this.jump;
+                      }
+                      break;
+             }
+           }
+
+  // Reset Hero
+  reset() {
+      // Set x and y to starting x and y
+      this.y = this.startY;
+      this.x = this.startX;
+  }
+}
+
+// Now instantiate your objects.
+  // New Hero object
+  const player = new Hero();
+  const bug1 = new Enemy(-101, 0, 200);
+  const bug2 = new Enemy(-101, 83, 300);
+  const bug3 = new Enemy((-101*2.5), 83, 300);
+  const allEnemies = [];
+  allEnemies.push(bug1,bug2,bug3);
+
+
+// Place all enemy objects in an array called allEnemies
+  // Init allEnemies array
+  // For each enemy create and push new Enemy object into above array
+
+// Place the player object in a variable called player
+
+
+
+// This listens for key presses and sends the keys to your
+// Player.handleInput() method. You don't need to modify this.
+document.addEventListener('keyup', function(e) {
+    var allowedKeys = {
+        37: 'left',
+        38: 'up',
+        39: 'right',
+        40: 'down'
+    };
+
+    player.handleInput(allowedKeys[e.keyCode]);
+});
